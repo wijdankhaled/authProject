a9d352017ec5db7846540a18a95b9bdfee10a2a4
Omar Alazaizeh <omx302@gmail.com>
2021-09-29 17:03:25 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,28 +1,30 @@
 import Auth from "./components/auth";
 import LoginProvider from "./context/context";
 import Login from "./components/login";
-
+import Students from "./components/users/Students";
+import Teacher from "./components/users/teachers";
+import Workers from "./components/users/allworkers";
+import Members from "./components/users/allmembers";
 export default function App(props) {
-    return (
-        <LoginProvider>
-            
-            <Login />
-            {/* user - editor - admin */}
-            <Auth capability="read">
-                <h2>you can read this!!</h2>
-            </Auth>
-            {/* editor - admin */}
-            <Auth capability="update">
-                <button>update</button>
-            </Auth>
-            {/* editor - admin */}
-            <Auth capability="create">
-                <button>create</button>
-            </Auth>
-            {/* admin */}
-            <Auth capability="delete">
-                <button>delete</button>
-            </Auth>
-        </LoginProvider>
-    )
+  return (
+    <LoginProvider>
+      <Login />
+      {/* user - editor - admin */}
+      <Auth capability="read">
+        <Students />
+      </Auth>
+      {/* editor - admin */}
+      <Auth capability="update">
+        <Teacher />
+      </Auth>
+      {/* editor - admin */}
+      <Auth capability="create">
+        <Workers />
+      </Auth>
+      {/* admin */}
+      <Auth capability="delete">
+        <Members />
+      </Auth>
+    </LoginProvider>
+  );
 }
---
+++ src/components/users/allmembers.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import faker from "faker";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  ListItemAvatar,
+  Avatar,
+  Checkbox,
+  Typography,
+  List,
+  Divider,
+  ListItem,
+} from "@mui/material";
+function Member() {
+  const [studentList, setStudent] = useState([]);
+
+  useEffect(() => {
+    setStudent(
+      new Array(20).fill(0).map((ele, index) => {
+        return (
+          <>
+            <ListItem
+              style={{
+                marginBottom: "20px",
+                background: "#F3F1F5",
+                borderRadius: "3rem",
+                margin: "0 5px",
+              }}
+            >
+              <ListItemAvatar>
+                <Avatar
+                  sx={{ width: 77, height: 77 }}
+                  alt="Travis Howard"
+                  src={faker.internet.avatar()}
+                />
+              </ListItemAvatar>
+              <Typography
+                paddingLeft={5}
+                gutterBottom
+                variant="h5"
+                component="div"
+              >
+                {faker.name.findName()}
+              </Typography>
+              <Checkbox inputProps={{ "aria-label": "controlled" }} />
+            </ListItem>
+            <Divider variant="inset" component="br" />
+          </>
+        );
+      })
+    );
+  }, []);
+
+  return (
+    <div>
+      <Typography
+        color="primary"
+        paddingLeft={5}
+        gutterBottom
+        variant="h2"
+        component="div"
+      >
+        Members
+      </Typography>
+      <Divider variant="inset" component="br" />
+      <List>{studentList}</List>
+    </div>
+  );
+}
+
+export default Member;
